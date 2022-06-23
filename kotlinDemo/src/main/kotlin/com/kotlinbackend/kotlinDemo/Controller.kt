40bffa92953afd577d6b885023b94db9bd66714d
Burhan TAŞKESEN <98235097+burhan-taskesen@users.noreply.github.com>
2022-06-23 22:34:23 +0300

    Add files via upload

---
+++ kotlinDemo/src/main/kotlin/com/kotlinbackend/kotlinDemo/Controller.kt
@@ -0,0 +1,14 @@
+package com.kotlinbackend.kotlinDemo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @GetMapping(value = arrayOf("/api"))
+    fun sayHi(): String {
+        return "hi";
+    }
+}
